fbb250c5bd59387ba1a4b39b074e2147d965b7e3
Islam Morsi <morsislam@gmail.com>
2015-06-07 11:12:58 -0700

    rails_apps_composer: set up database

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150607181243) do
+ActiveRecord::Schema.define(version: 20150607181250) do
 
   create_table "users", force: :cascade do |t|
     t.string   "name"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 20150607181243) do
     t.string   "uid"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "role"
   end
 
 end
